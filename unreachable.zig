cbbb9f3faa193a84946258d8059064c5aaf6056e
vj <vjlanguage@outlook.com>
2023-10-09 17:17:51 +0800

    Added new Zig files: enums, pointer, slicing, and unreachable with respective functions and tests

--- unreachable.zig
+++ unreachable.zig
@@ -4,3 +4,16 @@ pub fn main() !void {
     const y: u32 = if (x == 2) 5 else unreachable;
     std.debug.print("{}", .{y});
 }
+// Here is an unreachable being used in a switch.
+fn asciiToUppercase(x :u8) u8 {
+  return switch (x) {
+    'a'...'z' => x + 'A' - 'a',
+    'A'...'Z' => x,
+    else => unreachable,
+  };
+}
+
+test "unreachable switch" {
+  try std.testing.expect(asciiToUppercase('a') == 'A');
+  try std.testing.expect(asciiToUppercase('A') == 'A');
+}
