4f8fb94a6431a8eae2ba6b0393ed093d07cc7df5
vj <vjlanguage@outlook.com>
2023-10-20 15:32:18 +0800

    feat: Add makeDir function in file_system.zig for directory creation and file count

--- src/file_system.zig
+++ src/file_system.zig
@@ -32,8 +32,32 @@ fn readFileStats() !void {
     std.debug.print("ctime  = {d}\n", .{stat.atime});
 }
 
+fn makekDir() !void {
+    // if dir exists, delete it
+    std.fs.cwd().deleteTree("test-tmp") catch unreachable;
+    try std.fs.cwd().makeDir("test-tmp");
+    var iter_dir = try std.fs.cwd().openIterableDir("test-tmp", .{});
+    defer {
+        iter_dir.close();
+        std.fs.cwd().deleteTree("test-tmp") catch unreachable;
+    }
+    _ = try iter_dir.dir.createFile("x", .{});
+    _ = try iter_dir.dir.createFile("y", .{});
+    _ = try iter_dir.dir.createFile("z", .{});
+
+    var file_count: usize = 0;
+    var iter = iter_dir.iterate();
+    while (try iter.next()) |entry| {
+        if (entry.kind == .file) {
+            file_count += 1;
+        }
+    }
+    std.debug.print("file_count = {d}\n", .{file_count});
+}
+
 pub fn main() !void {
     // try createFile();
     // try readFile();
-    try readFileStats();
+    // try readFileStats();
+    try makekDir();
 }
